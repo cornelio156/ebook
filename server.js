5594a6ccadbfac79e5b51067b441951b30427796
cornelio156 <fernandosilvanositoe2008@gmail.com>
2025-10-01 15:37:32 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ const distPath = path.join(__dirname, 'dist');
 app.use(express.static(distPath));
 
 // SPA fallback
-app.get('*', (req, res, next) => {
+app.get('/*', (req, res, next) => {
   if (req.path.startsWith('/api')) return next();
   res.sendFile(path.join(distPath, 'index.html'));
 });
